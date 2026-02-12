4a6241fe3817d7f4c510ea4b8b586faafb3ec929
jho-G <yohanneshailegiorgis0@gmail.com>
2026-02-12 15:44:28 +0300

    Refactor game logic for Anton and Danik

--- anton and danik.py
+++ anton and danik.py
@@ -1,11 +1,10 @@
-import re
-n = input()
-s = input()
-Danik = len(re.findall(r'D', s))
-Anton = len(s)  - Danik
-if(Danik > Anton):
-  print("Danik")
-elif(Danik == Anton):
-  print("Friendship")
+num_of_game=int(input())
+str1=input().strip()
+count_A=str1.count('A')
+count_D=str1.count('D')
+if count_A>count_D:
+    print("Anton")
+elif count_D>count_A:
+    print("Danik")
 else:
-  print("Anton")
+    print("Friendship")
