8d57606b4c129b4b3c58f7ece173d303904a298a
jho-G <yohanneshailegiorgis0@gmail.com>
2026-02-25 09:07:10 +0300

    Create shaass and oscoles.py

---
+++ shaass and oscoles.py
@@ -0,0 +1,22 @@
+n = int(input())
+a = list(map(int, input().split()))
+m = int(input())
+
+for _ in range(m):
+    x, y = map(int, input().split())
+    
+    x -= 1  # convert to 0-based index
+    
+    left_birds = y - 1
+    right_birds = a[x] - y
+    
+    if x > 0:
+        a[x - 1] += left_birds
+    
+    if x < n - 1:
+        a[x + 1] += right_birds
+    
+    a[x] = 0
+
+for birds in a:
+    print(birds)
